c5dba809faa2b2282984b4cf3b761ce2e9dd15f2
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-02 22:55:53 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -39,7 +39,7 @@ describe 'form' do
     visit edit_post_path(@post)
 
     # fill_in :title, with: "My edit"
-    fill_in 'description', with: "My post description"
+    # fill_in 'description', with: "My post description"
 
     click_on "Submit Post"
 
